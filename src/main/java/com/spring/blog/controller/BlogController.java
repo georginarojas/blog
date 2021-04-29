0dfec68c99e790ebc921b672713b1f2b6922bb89
Georgina Rojas <ginarojas29@gmail.com>
2021-04-29 11:33:29 -0300

    Creating the Newpost page.

--- src/main/java/com/spring/blog/controller/BlogController.java
+++ src/main/java/com/spring/blog/controller/BlogController.java
@@ -4,11 +4,15 @@ import com.spring.blog.model.Post;
 import com.spring.blog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -18,21 +22,37 @@ public class BlogController {
     BlogService blogService;
 
     @RequestMapping(value = "/posts", method = RequestMethod.GET)
-    public ModelAndView getPosts(){
-        ModelAndView mv= new ModelAndView("posts");
+    public ModelAndView getPosts() {
+        ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = blogService.findAll();
         mv.addObject("posts", posts);
         return mv;
     }
 
     @RequestMapping(value = "/post/{id}", method = RequestMethod.GET)
-    public  ModelAndView getPostDetails(@PathVariable("id") long id){
+    public ModelAndView getPostDetails(@PathVariable("id") long id) {
         ModelAndView mv = new ModelAndView("postDetails");
         Post post = blogService.findById(id);
         System.out.println(">>>>> ID");
         System.out.println(id);
         mv.addObject("post", post);
         return mv;
+    }
+
+    //    Return the postForm page
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm() {
+        return "postForm";
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            return "redirect:/newpost";
+        }
+        post.setDate(LocalDate.now());
+        blogService.save(post);
+        return "redirect:/posts";
     }
 
 }
